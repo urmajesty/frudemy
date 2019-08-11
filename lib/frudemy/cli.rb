ab20f9df833aa03ec2d271dbbb5c1eba6db1ba1c
github username <chyrellelewis@gmail.com>
2019-08-10 22:24:12 -0500

    right before I put actual values in

--- lib/frudemy/cli.rb
+++ lib/frudemy/cli.rb
@@ -5,21 +5,26 @@ class Frudemy::CLI
         menu
         see_ya
     end
+   
     def list_courses
+
         puts "Today's Free Courses"
         @courses = Frudemy::Course.today
+        @courses.each.with_index(1) do |course, i|
+            puts "#{i}. #{course.name} - #{course.category} - #{course.clicks}"
+        end
     end
+    
     def menu
         input = nil
         while input != "exit"
             puts "Enter the number of the course you're interested in or type list to see the deals again, or type exit."
         input = gets.strip.downcase
-        case input
-        when "1" 
-            puts "More info on course 1..."
-        when "2"
-            puts "More info on course 2..."
-        when "list"
+        
+        if input.to_i > 0 
+            the_course = @courses[input.to_i-1]
+            puts "#{course.name} - #{course.category} - #{course.clicks}"
+        elsif input == "list"
             list_courses
         else
             puts "Not sure what you need.  Please type list or exit."
