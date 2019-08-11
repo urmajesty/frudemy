485fda45da725246403365e5861763b6f5243920
github username <chyrellelewis@gmail.com>
2019-08-10 21:12:26 -0500

    spec issues

--- lib/frudemy.rb
+++ lib/frudemy.rb
@@ -1,6 +1,6 @@
 require_relative "frudemy/version"
 require_relative "./frudemy/cli"
-
+require_relative "./frudemy/course"
 
 module Frudemy
   class Error < StandardError; end
--- lib/frudemy/course.rb
+++ lib/frudemy/course.rb
@@ -1,9 +1,24 @@
 class Frudemy::Course
+    attr_accessor :name, :category, :clicks, :url
     def self.today
         puts <<-DOC.gsub /^\s*/, ''
             1. UDEMY COURSE 1
             2. UDEMY COURSE 2
         DOC
-
+        course_1 = self.new
+        course_1.name = "udemy course 1"
+        course_1.category = "ruby"
+        course_1.clicks = clicks
+        course_1.url = "https//happy.com"
     end
+
+        course_2= self.new
+        course_2.name = "udemy course 2"
+        course_2.category = "javascript"
+        course_2.clicks = clicks
+        course_2.url = "https//www.code.com"
+
+        [course_1, course_2]
+    end
+
 end
