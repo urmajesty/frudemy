afc7ed7f6e5bf3dfea7da61a48a1672b12b2ad6f
github username <chyrellelewis@gmail.com>
2019-08-11 18:00:55 -0500

    testing scraping function

--- lib/frudemy/course.rb
+++ lib/frudemy/course.rb
@@ -2,19 +2,23 @@ class Frudemy::Course
     attr_accessor :name, :category, :clicks, :url
     def self.today
         
-        course_1 = self.new
-        course_1.name = "udemy course 1"
-        course_1.category = "ruby"
-        course_1.clicks = "click"
-        course_1.url = "https//happy.com"
+        self.scraping
+    end
 
-        course_2 = self.new
-        course_2.name = "udemy course 2"
-        course_2.category = "javascript"
-        course_2.clicks = "click"
-        course_2.url = "https//www.code.com"
+    def self.scraping
+        courses = []
+        
+        courses << self.scraping_udemy
 
-        [course_1, course_2]
+
+
+
+        courses
+    end
+
+    def self.scraping_udemy
+        doc = Nokogiri::HTML(open("https://discudemy.com"))
+        binding.pry
     end
 
 end
