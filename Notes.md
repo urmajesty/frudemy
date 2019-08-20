15f998fefb0b8e6ea2ee58a4a2cd74480b0c5a77
github username <chyrellelewis@gmail.com>
2019-08-20 13:19:45 -0500

    finished frudemy

--- Notes.md
+++ Notes.md
@@ -49,3 +49,25 @@ end
             detail = course_card.css("a.card-header").text
           
      id = index + 1
+
+     def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the deal you'd like more info on or type list to see the deals again or type exit:"
+      input = gets.strip.downcase
+
+      if input.to_i > 0
+        the_deal = @deals[input.to_i-1]
+        puts "#{the_deal.name} - #{the_deal.price} - #{the_deal.availability}"
+      elsif input == "list"
+        list_deals
+      else
+        puts "Not sure what you want, type list or exit."
+      end
+    end
+  end
+
+  def goodbye
+    puts "See you tomorrow for more deals!!!"
+  end
+end
--- lib/frudemy/cli.rb
+++ lib/frudemy/cli.rb
@@ -1,13 +1,13 @@
 class Frudemy::CLI
 
   def start
-    puts "Welcome to Frudemy!!!"
-    puts "Please choose the category you are interested in learning"
+    puts "WELCOME TO FRUDEMY!!!".colorize(:green)
+    puts "\nPlease choose a category you're interested in learning by selecting a number:".colorize(:green)
     Frudemy::Scrape.scrape_categories
     set_category_roster
- 
     list_categories
     choose_category
+   
   end
 
   def set_category_roster
@@ -16,7 +16,7 @@ class Frudemy::CLI
 
   def list_categories
     @roster.each.with_index(1) do |category, index|
-      puts "#{index}. #{category.category_name}"
+      puts "#{index}. #{category.category_name.upcase}"
       puts 
 
     end
@@ -49,10 +49,28 @@ class Frudemy::CLI
 
     puts "This category has the following courses:"
     #gets just gets just relavent courses
-    category.course.each do |course|
+    category.course.each.with_index(1) do |course, index|
     
-      puts "\n\nName: #{course.course_name}. \nDate_added: #{course.date_added} \nDescription: #{course.description}. \nURL: #{course.url}"
+      puts "\n\n#{index}. Name: #{course.course_name.upcase}".colorize(:green)
+      puts "Date added: #{course.date_added}\nDescription: #{course.description}.\nURL: #{course.url}"
     end
+  second_menu
+  end
+
+
+ def second_menu
+   puts "\n\nWould you like to look at another category? Type 'C'"
+   puts "\n\nWould you like to exit? Type 'E'"
+   input = gets.strip.upcase
+    if input == "C"
+     list_categories
+     choose_category
+    elsif input == "E"
+     puts "We update courses often.  Please come again soon."
+    else
+     puts "Sorry.  I didn't understand that command".colorize(:red)
+     second_menu
+   end
   end
 end
 
